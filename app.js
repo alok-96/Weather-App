5592841a4c011485e8ecab942bb3a4c413e67ac4
alok96 <dwivedialok799@gmail.com>
2023-04-04 18:59:34 +0530

    bug fix

--- app.js
+++ app.js
@@ -7,23 +7,35 @@ const searchIcon = document.getElementById("search-icon");
 const weatherImg = document.querySelector(".weather-img");
 const errorMessage = document.querySelector(".error");
 
+// Displaying weather box
+const displayWeatherBox = function () {
+  errorMessage.classList.add("hidden");
+  document.querySelector(".weather-info").classList.remove("hidden");
+  document.querySelector(".weather-details").classList.remove("hidden");
+};
+
+// Hiding weather box
+const hideWeatherBox = function () {
+  errorMessage.classList.remove("hidden");
+  document.querySelector(".weather-info").classList.add("hidden");
+  document.querySelector(".weather-details").classList.add("hidden");
+  errorMessage.innerHTML = "❌ Invalid City Name";
+};
+
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   const data = await response.json();
 
   console.log(data);
-  if (data.message == "city not found") {
-    errorMessage.classList.remove("hidden");
-    document.querySelector(".weather-info").classList.add("hidden");
-    document.querySelector(".weather-details").classList.add("hidden");
-} else {
-    document.querySelector(".weather-info").classList.remove("hidden");
-    document.querySelector(".weather-details").classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-
-    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + " °C";
-    document.querySelector(".weather-condition").innerHTML = data.weather[0].description;
-    document.querySelector(".city-name").innerHTML = data.name + ", " + data.sys.country;
+  if (data.message == "city not found") hideWeatherBox();
+  else {
+    displayWeatherBox();
+    document.querySelector(".temperature").innerHTML =
+      Math.round(data.main.temp) + " °C";
+    document.querySelector(".weather-condition").innerHTML =
+      data.weather[0].description;
+    document.querySelector(".city-name").innerHTML =
+      data.name + ", " + data.sys.country;
     document.querySelector("#humidity").innerHTML = data.main.humidity + "%";
     document.querySelector("#wind-speed").innerHTML = data.wind.speed + "km/h";
     if (data.weather[0].main == "Clouds") weatherImg.src = "assets/clouds.png";
@@ -38,10 +50,32 @@ async function checkWeather(city) {
 }
 
 searchIcon.addEventListener("click", () => {
-    checkWeather(city.value);
+  if (city.value == "") {
+    hideWeatherBox();
+    errorMessage.innerHTML = "⚠️ Please Enter A City Name";
+  } else checkWeather(city.value);
 });
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Enter")
-  checkWeather(city.value);
+  if (e.key === "Enter") {
+    if (city.value == "") {
+      hideWeatherBox();
+      errorMessage.innerHTML = "⚠️ Please Enter A City Name";
+    } else checkWeather(city.value);
+  }
+});
+
+// Typed top bar
+var typed = new Typed(".typing-text", {
+  strings: [
+    "Sunshine is delicious,",
+    "Rain is refreshing,",
+    " Wind braces us up,",
+    "Snow is exhilarating,",
+    "There is really no such thing as bad weather,",
+    "Only different kinds of good weather.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
